241ee1545f2453330e0fd0e837a3ad4959c58b65
pexwave <asakilsarhan@gmail.com>
2024-03-04 19:39:54 +0800

    color update

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -4,61 +4,25 @@ class TColors {
   TColors._();
 
   //App Basic Colors
-  static const Color primary = Color(0xFFC12F2F);
-  static const Color secondary = Color(0xFFD86E6E);
-  static const Color accent = Color(0xFFeea93a);
+  static const Color primary = Color(0xFFDB2424);
+  static const Color secondary = Color(0xFF1A1A1A);
+  static const Color accent = Color(0xFFEDD712);
 
-  //Gradient Color
-  static const Gradient linearGradient = LinearGradient(
-    begin: Alignment(0.0, 0.0),
-    end: Alignment(0.707, -0.707),
-    colors: [
-        Color(0xffff9a93),
-        Color(0xfffad0c4),
-        Color(0xfffad0c4),
-      ]
-  );
-
-  //Text Color
-  static const Color textPrimary = Color(0xFF120907);
-  static const Color textSecondary = Color(0xFF6c757d);
-  static const Color textWhite = Colors.white;
-
-  //Backgound Color
-  static const Color light = Color(0xFFfdf7fa);
-  static const Color dark = Color(0xFF272727);
-  static const Color primaryBackground = Color(0xFFF3f5ff);
-
-  //Background Container Colors
-  static const Color lightContainer = Color(0xFFf6f6f6);
-  static Color darkContainer = TColors.white.withOpacity(0.1);
-
-  // Button Colors
-  static const Color buttonPrimary = Color(0xFF4b68ff);
-  static const Color buttonSecondary = Color(0xFF6c757d);
-  static const Color buttonDisabled = Color(0xFFc4c4c4);
+  static const Color primary600 = Color(0xFFF57070);
+  static const Color primary300 = Color(0xFFFCCFCF);
+  static const Color primary100 = Color(0xFFFDE7E7);
 
 
-  // Border Colors
-  static const Color borderPrimary = Color(0xFFd9d9d9);
-  static const Color borderSecondary = Color(0xFFe6e6e6);
-
-  // Error and Validation Colors
-
-  static const Color error = Color(0xFFd32f2f);
-  static const Color success = Color(0xFF388e3c);
-  static const Color warning = Color(0xFFf57c00);
-  static const Color info = Color(0xFF1976d2);
+  static const Color secondary600 = Color(0xFF737373);
+  static const Color secondary300 = Color(0xFFD9D9D9);
+  static const Color secondary100 = Color(0xFFF2F2F2);
 
 
-  // Neutral Shades
-  static const Color black = Color(0xFF232323);
-  static const Color darkerGrey = Color(0xFF4f4f4f);
-  static const Color darkGrey = Color(0xFF939393);
-  static const Color grey = Color(0xFFe0e0e0);
-  static const Color softGrey = Color(0xFFf4f4f4);
-  static const Color lightGrey = Color(0xFFf9f9f9);
-  static const Color white = Color(0xFFFFFFFF);
+  static const Color accent600 = Color(0xFFF4E771);
+  static const Color accent300 = Color(0xFFFAF3B8);
+  static const Color accent100 = Color(0xFFFBF7D0);
+
+
 
 
 }
